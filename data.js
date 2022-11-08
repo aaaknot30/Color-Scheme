53b7400a31cacd184ba46428fc724c382e2eb640
Kyle Larson <aaaknot30@yahoo.com>
2022-11-07 22:31:47 -0700

    Added Files

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        key: "Monochrome",
+        value: "monochrome"
+    },
+    {
+        key: "Monochrome-light",
+        value: "monochrome-light"
+    },
+    {
+        key: "Monochrome-dark",
+        value: "monochrome-dark"
+    },
+    {
+        key: "Analogic",
+        value: "analogic"
+    },
+    {
+        key: "Complement",
+        value: "complement"
+    },
+    {
+        key: "Analogic-complement",
+        value: "analogic-complement"
+    },
+    {
+        key: "Triad",
+        value: "triad"
+    }
+]
+export {data}
---
+++ index.js
@@ -0,0 +1,97 @@
+import { data } from "/data.js"
+
+
+// ---------   Step 1: Fecth data -----------
+document.getElementById("color--pick").value = "#FF7700"
+let colorScheme = "monochrome"
+function fetchColors() {
+    let color = document.getElementById("color--pick").value.slice(1)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${colorScheme}&count=5`)
+        .then(res => res.json())
+        .then(data => {
+            parse(data)
+        })
+}
+fetchColors()
+
+// ------------- Step 2: Parse data -----------
+let colorsArray = []
+function parse(data) {
+    data.colors.forEach((item) => {
+        colorsArray.push(item.name.closest_named_hex)
+    })
+    render()
+}
+
+// --------- Step 3. Render dropdown menu ---------
+let selectedColor = "Monochrome"
+let checkMark = `<img class="svg" src="checkmark_trans.png">`
+function renderDropDown() {
+    let htmlData = ""
+    data.forEach((item) => {
+        htmlData +=
+            `<li id="option" data-key="${item.key}" data-value="${item.value}" class="select__item">
+            <span id="option" data-key="${item.key}" data-value="${item.value}">${item.key}</span>
+            ${selectedColor == item.key ? checkMark : ""} 
+        </li>`
+    })
+    document.getElementById("color--scheme").innerHTML =
+        `<div id="select" class="select">
+            <div id="select__input" class="select__input">
+                <span id="select__input">${selectedColor}</span>   
+                <img id="select__input" class="svg" src="down_arrow_trans.png">
+            </div> 
+            <ul id="select__options" class="select__options">
+            ${htmlData}
+            </ul>
+        </div>`
+}
+renderDropDown()
+
+// Step 4. ---------- Render color palette ------------------
+function render() {
+    document.getElementById("color1").style.background = colorsArray[0]
+    document.getElementById("color1--name").innerHTML = colorsArray[0]
+    document.getElementById("color2").style.background = colorsArray[1]
+    document.getElementById("color2--name").innerHTML = colorsArray[1]
+    document.getElementById("color3").style.background = colorsArray[2]
+    document.getElementById("color3--name").innerHTML = colorsArray[2]
+    document.getElementById("color4").style.background = colorsArray[3]
+    document.getElementById("color4--name").innerHTML = colorsArray[3]
+    document.getElementById("color5").style.background = colorsArray[4]
+    document.getElementById("color5--name").innerHTML = colorsArray[4]
+    colorsArray = []
+}
+
+// Step 5. ---------- Define dropdown toggle ------------------
+function toggleDropDown(drop) {
+    //console.log(drop)
+    if (drop) {
+        document.getElementById("select__options").style.visibility = "visible"
+        document.getElementById("select__options").style.opacity = "1"
+    } else {
+        document.getElementById("select__options").style.visibility = "hidden"
+        document.getElementById("select__options").style.opacity = "0"
+    }
+}
+
+// Step 6. ---------- Define event listeners ------------------
+document.addEventListener("click", (e) => {
+    if (e.target.id == "select__input") {
+        toggleDropDown(true)
+    } else if (e.target.id == "option") {
+        toggleDropDown(false)
+        selectedColor = e.target.dataset.key
+        colorScheme = e.target.dataset.value
+        renderDropDown()
+        fetchColors()
+    }
+})
+
+document.getElementById("btn").addEventListener("click", function (e) {
+    e.preventDefault();
+    fetchColors()
+})
+
+
+
